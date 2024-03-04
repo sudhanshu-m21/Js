c36089826a1af9c862d576522b3e73c4b53754ba
sudhanshu-m21 <139671253+sudhanshu-m21@users.noreply.github.com>
2024-03-04 11:18:58 +0000

    data type

---
+++ JS/04.js
@@ -0,0 +1,10 @@
+//primitive and non primitive data type
+
+
+//primitive = string,number,boolean,null,undefined,symbol,BigInt
+
+
+//non: array,object,function
+
+
+const num = 1000000000000000000555n  //big int
