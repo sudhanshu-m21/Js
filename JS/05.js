b38dfc9517f97a0d8af7bcb301ebae4f9a695197
sudhanshu-m21 <139671253+sudhanshu-m21@users.noreply.github.com>
2024-03-06 09:40:35 +0000

    math and num

---
+++ JS/05.js
@@ -0,0 +1,21 @@
+const name = "sudhanshu"
+const repocnt = 20
+
+// console.log(name + repocnt);
+// console.log(`Hello my name is ${name} and my repo count is ${repocnt}`);
+
+const gamename = "Sudhanshu-M"
+const newstring = gamename.substring(0,4)
+
+// console.log(newstring);
+
+const anotherstring = gamename.slice(-11,4)
+// console.log(anotherstring);
+
+const newstringone = "  smishra   "
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+const url = "https://smishra%51.com"
+
+console.log(url.replace('%51','-'))
---
+++ JS/06.js
@@ -0,0 +1,10 @@
+const score = 400 
+
+const num = 123.897
+console.log(num.toPrecision(3));
+
+const hndr = 1000000
+
+console.log(hndr.toLocaleString('en-IN'));
+
+//math.random give value between 0 and 1
