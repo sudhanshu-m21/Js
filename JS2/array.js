555e91bb3a4414e64f6b9aca223d869829eb857d
sudhanshu-m21 <139671253+sudhanshu-m21@users.noreply.github.com>
2024-03-09 10:08:12 +0000

    array

---
+++ JS2/array.js
@@ -0,0 +1,47 @@
+const arr = [1, 2, 3, 4, 5, 6];
+const arr2 = ["Ram", "shyam"];
+
+// console.log(arr);
+
+// arr.push(7)
+// console.log(arr);
+// arr.pop()
+// arr.unshift(7);
+// arr.shift()
+// console.log(arr.includes(6));
+// console.log(arr.indexOf(6));
+
+const arr3 = arr.join(); // change type fron arr to string;
+
+// console.log(arr);
+// console.log(arr3);
+
+const marvel_heros = ["thor", "Ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({ name: "hitesh" })); // interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
