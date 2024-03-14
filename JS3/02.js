1f04fb62d528c9a8a37981b1a2268ba0a01ec43a
sudhanshu-m21 <139671253+sudhanshu-m21@users.noreply.github.com>
2024-03-14 11:35:21 +0000

    scop

---
+++ JS3/02.js
@@ -0,0 +1,25 @@
+let a = 100;
+if (true) {
+  let a = 200;
+}
+
+console.log(a);
+
+
+//interesting concept
+
+
+function addone(num)
+{
+  return num+1
+}
+
+addone(15)
+
+
+const addTwo = function(num)
+{
+  return num+2;
+}
+
+addTwo(5);
